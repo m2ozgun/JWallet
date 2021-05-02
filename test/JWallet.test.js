731deca0155083e4a043e12c6bbecaa0f81390ef
Your Name Here <memozgun@gmail.com>
2021-05-02 08:05:10 +0300

    test(wallet): balance increase after deposit

--- test/JWallet.test.js
+++ test/JWallet.test.js
@@ -1,7 +1,19 @@
 
 const JWallet = artifacts.require('JWallet')
 
-contract('JWallet', async () => {
+
+var chai = require("chai");
+
+const BN = web3.utils.BN;
+const chaiBN = require('chai-bn')(BN);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract('JWallet', async accounts => {
     let walletInstance;
 
     beforeEach(async () => {
@@ -10,8 +22,13 @@ contract('JWallet', async () => {
 
     it('wallet balance should start with 0', async () => {
         let balance = await web3.eth.getBalance(walletInstance.address)
-        console.log('balance' + balance + ' ' +  walletInstance.address)
-        assert.equal(balance, 0)
+        expect(web3.eth.getBalance(walletInstance.address)).to.eventually.be.a.bignumber.equal(new BN(0))
+    })
+    
+    it('balance should increase after deposit', async () => {
+        await web3.eth.sendTransaction({ from: accounts[0], to: walletInstance.address, value: 1})
+        expect(web3.eth.getBalance(walletInstance.address)).to.eventually.be.a.bignumber.equal(new BN(1))
+
     })
 
 })
