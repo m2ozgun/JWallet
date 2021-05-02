152eee8a110f2719a8f9b8a2a18d39de4c9f8cdb
Your Name Here <memozgun@gmail.com>
2021-05-02 10:35:12 +0300

    refactor(wallet): improve allowance

--- contracts/JWallet.sol
+++ contracts/JWallet.sol
@@ -1,13 +1,18 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.4;
 
 import "./Allowance.sol";
 
 contract JWallet is Allowance {
+    event AllowanceIncreased(address indexed _address, uint _amount);
+    event AllowanceDecreased(address indexed _address, uint _amount);
+
     function withdraw(address payable _to, uint256 _amount) public isAllowed(_amount) {
-        require(_amount < address(this).balance, "Wallet amount is not sufficient.");
+        
+        emit AllowanceDecreased(_to, _amount);
         _to.transfer(_amount);
     }
 
-
-    receive() external payable {}
+    receive() external payable {
+        emit AllowanceIncreased(msg.sender, msg.value);
+    }
 }
