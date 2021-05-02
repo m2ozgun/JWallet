7b7b7dcf4bf571a822c5f41bff1c458f3315496f
Your Name Here <memozgun@gmail.com>
2021-05-02 09:44:21 +0300

    refactor(allowance): extract to new contract

---
+++ contracts/Allowance.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable {
+    event AllowanceEvent(address indexed _of, address indexed _by, uint _prevAmount, uint _newAmount);
+    mapping(address => uint256) allowance;
+
+    function isOwner() internal view returns (bool) {
+        return owner() == msg.sender;
+    }
+
+    modifier isAllowed(uint256 _amount) {
+        require(
+            allowance[msg.sender] >= _amount || isOwner(),
+            "Address does not have sufficient funds."
+        );
+        _;
+    }
+
+    function setAllowance(address _address, uint _amount) public onlyOwner {
+        emit AllowanceEvent(_address, msg.sender, allowance[_address], _amount);
+        allowance[_address] = _amount;
+    }
+
+    function reduceAllowance(address _address, uint _amount) public isAllowed(_amount) {
+        emit AllowanceEvent(_address, msg.sender, allowance[_address], allowance[_address] - _amount);
+
+        allowance[_address] -= _amount;
+    }
+}
--- contracts/JWallet.sol
+++ contracts/JWallet.sol
@@ -1,26 +1,13 @@
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "./Allowance.sol";
 
-contract JWallet is Ownable {
-    mapping(address => uint) allowance;
-
-    function isOwner() internal view returns(bool) {
-        return owner() == msg.sender;
-    }
-
-    modifier isAllowed(uint _amount) {
-        require(allowance[msg.sender] >= _amount || isOwner(), "Address does not have sufficient funds."); 
-        _;
-    }
-
-    function withdraw(address payable _to, uint _amount) public isAllowed(_amount){
+contract JWallet is Allowance {
+    function withdraw(address payable _to, uint256 _amount) public isAllowed(_amount) {
         require(_amount < address(this).balance, "Wallet amount is not sufficient.");
-
         _to.transfer(_amount);
     }
 
-    receive() external payable {
 
-    }
+    receive() external payable {}
 }
